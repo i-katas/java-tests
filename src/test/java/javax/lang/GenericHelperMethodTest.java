9ae84e6ca133386314c7219e30b8235ca517a5d3
i-katas <feexonj@gmail.com>
2019-08-24 22:58:24 +0800

    solve generic wildcard capture error in compile stage by introduce a helper method

---
+++ src/test/java/javax/lang/GenericHelperMethodTest.java
@@ -0,0 +1,49 @@
+package javax.lang;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author xiaoliang
+ * @since 2019-08-24
+ */
+public class GenericHelperMethodTest {
+
+    @Test
+    public void swapElementsForUpperBoundedParameterizedType() {
+        Point<Integer> point = new Point<>(1, 3);
+        assertThat(point.x, equalTo(1));
+        assertThat(point.y, equalTo(3));
+
+        rotate(point);
+
+        assertThat(point.x, equalTo(3));
+        assertThat(point.y, equalTo(1));
+    }
+
+    /**
+     * The wildcard capture error raised by compiler if inline {@link GenericHelperMethodTest#rotateHelper} method.
+     * @see <a href="Wildcard Capture and Helper Methods">https://docs.oracle.com/javase/tutorial/java/generics/capture.html</a>
+     * @param point
+     */
+    private void rotate(Point<?> point) {
+        rotateHelper(point);
+    }
+
+    private <T extends Number> void rotateHelper(Point<T> p) {
+        T x = p.x;
+        p.x = p.y;
+        p.y = x;
+    }
+
+    static class Point<T extends Number> {
+        T x, y;
+
+        Point(T x, T y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
