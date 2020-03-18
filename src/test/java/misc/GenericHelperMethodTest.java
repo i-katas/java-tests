6b0929304cb4fa7a3d8997e1e6852e6ec00502ba
i-katas <feexonj@gmail.com>
2020-03-18 17:42:15 +0800

    fix test

--- src/test/java/misc/GenericHelperMethodTest.java
+++ src/test/java/misc/GenericHelperMethodTest.java
@@ -13,33 +13,30 @@ public class GenericHelperMethodTest {
 
     @Test
     public void swapElementsForUpperBoundedParameterizedType() {
-        Point<Integer> point = new Point<>(1, 3);
+        Point<?> point = new Point<>(1, 3);
         assertThat(point.x, equalTo(1));
         assertThat(point.y, equalTo(3));
 
-        rotate(point);
+        //point.x = point.y; //error
+        swap(point);
 
         assertThat(point.x, equalTo(3));
         assertThat(point.y, equalTo(1));
     }
 
     /**
-     * The wildcard capture error raised by compiler if inline {@link GenericHelperMethodTest#rotateHelper} method.
+     * The wildcard capture error raised by compiler if inline {@link GenericHelperMethodTest#swap} method.
      *
      * @param point
      * @see <a href="Wildcard Capture and Helper Methods">https://docs.oracle.com/javase/tutorial/java/generics/capture.html</a>
      */
-    private void rotate(Point<?> point) {
-        rotateHelper(point);
+    private <T> void swap(Point<T> point) {
+        T x = point.x;
+        point.x = point.y;
+        point.y = x;
     }
 
-    private <T extends Number> void rotateHelper(Point<T> p) {
-        T x = p.x;
-        p.x = p.y;
-        p.y = x;
-    }
-
-    static class Point<T extends Number> {
+    static class Point<T> {
         T x, y;
 
         Point(T x, T y) {
